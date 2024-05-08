7b0003f7026bd534cc9559d9fc32d3198d8279dc
i-Milman <ivan.milman.arkhangelsk@ya.ru>
2024-05-08 19:13:25 +0300

    Add files via upload
    Добавлены комментарии

--- task1_0.py
+++ task1_0.py
@@ -1,7 +1,10 @@
+#Исходные данные:
 stroka = 'это моя строка'
-print (stroka[0])
-print (stroka[-1])
-print (stroka[2:5])
-print (stroka[::-1])
-print (len(stroka))
-print ('это новая строка' + stroka)
+
+#Решение:
+print (stroka[0])                   #1
+print (stroka[-1])                  #2
+print (stroka[2:5])                 #3
+print (stroka[::-1])                #4
+print (len(stroka))                 #5
+print ('это новая строка' + stroka) #6 (я бы добавил пробел, но в условии сказано сделать именно так)
