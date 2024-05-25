679cc9dfd0594f3c9c6970e2741d41b6b378df55
i-Milman <ivan.milman.arkhangelsk@ya.ru>
2024-05-26 02:48:56 +0300

    Add files via upload

---
+++ M7T3.py
@@ -0,0 +1,50 @@
+from random import randint
+
+
+class Team:
+
+    def __init__(self, name):
+        self.name = name
+        self.num = randint(4,6)
+        self.score = 0
+        self.time = 0
+
+    def solve_tasks(self):
+        self.score = randint(40,50)
+        self.time = randint (240, 600) * self.score
+
+
+team1 = Team('Мастера кода')
+team2 = Team('Волшебники данных')
+
+# Использование `%`
+print('В команде %s участников: %d !' % (team1.name, team1.num))
+print('В команде %s участников: %d !' % (team2.name, team2.num))
+print('Итого сегодня в командах участников : %d и %d !' % (team1.num, team2.num))
+print()
+
+team1.solve_tasks()
+team2.solve_tasks()
+
+# Использование `format()`:
+print('Команда {} решила задач: {} !'.format(team1.name, team1.score))
+print('{} решили задачи за {} с !'.format(team1.name, team1.time))
+print('Команда {} решила задач: {} !'.format(team2.name, team2.score))
+print('{} решили задачи за {} с !'.format(team2.name, team2.time))
+print()
+
+# Использование f-строк:
+print(f'Команды решили {team1.score} и {team2.score} задач.')
+
+if team1.score > team2.score or team1.score == team2.score and team1.time > team2.time:
+    challenge_result = f'Победа команды {team1.name}!'
+elif team1.score < team2.score or team1.score == team2.score and team1.time < team2.time:
+    challenge_result = f'Победа команды {team2.name}!'
+else:
+    challenge_result = 'Ничья!'
+print(f'Результат битвы: {challenge_result}')
+print()
+
+tasks_total = team1.score + team2.score
+time_avg = (team1.time + team2.time) / tasks_total
+print(f'Сегодня было решено {tasks_total} задач, в среднем по {time_avg:.1f} секунды на задачу!')
